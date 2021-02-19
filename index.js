d1fcbbad67b4a43d50db4b2d78424983be3a05c7
Maxim Matyunin <matyunya@gmail.com>
2021-02-19 18:51:45 +0900

    fixes on initial load

--- index.js
+++ index.js
@@ -10,29 +10,45 @@ const onObserve = (process, filterClasses, appendStyle, appendStyleMedia) => mut
       .filter(t => t.type === 'attributes')
       .map(i => (i.target.classList.value || "").split(' '))
       .flat()
-      .filter(filterClasses)
+      .filter(a => {
+        return filterClasses(a)
+      })
   )]
     .map(process)
     .filter(Boolean);
 
-  styles = [...styles, [...new Set(
+  styles = [...styles, ...[...new Set(
     mutations
-      .filter(f => f.type === 'childList' && f.addedNodes.length && f.addedNodes[0].classList) // only works on a single node
-      .map(i => (i.addedNodes[0].classList.value || "").split(' ')).flat().filter(filterClasses))
+      .filter(f => {
+        return f.type === 'childList' && f.addedNodes.length && f.addedNodes[0].classList
+      })
+      .map(i => {
+        let classes = [];
+        const node = i.addedNodes[0]
+        const all = node.getElementsByTagName('*');
+
+        for (var i = -1, l = all.length; ++i < l;) {
+            classes.push((all[i].classList.value ||'').split(' '));
+        }
+        return [...classes.flat(), ...(node.classList.value || '').split(' ')];
+      })
+      .flat()
+    )
   ]
+    .filter(filterClasses)
     .map(process)
-    .filter(Boolean)];
+    .filter(Boolean)
+  ];
 
   appendStyle(styles.filter(s => !s.includes('@media')).join('\n'));
   appendStyleMedia(styles.filter(s => s.includes('@media')).join('\n'));
 }
 
-const getInitialClasses = (process, filterClasses) => [...new Set(
+const getInitialClasses = (process) => [...new Set(
   [...document.querySelectorAll("*")]
     .map(i => i.classList.value.split(' '))
     .flat()
 )]
-  .filter(filterClasses)
   .map(process)
   .filter(Boolean);
 
@@ -73,6 +89,8 @@ function mergeUserConfig(config, userConfig) {
 }
 
 function appendCssToEl(css, el) {
+  if (!css) return;
+
   if (el.styleSheet) {
     el.styleSheet.cssText = css;
   } else {
@@ -97,24 +115,25 @@ export function init(userConfig, container = document.querySelector('body')) {
   }
 
   const classes = new Set();
-  const filterClasses = i => Boolean(i) && !i.startsWith('svelte-') && !classes.has(i);
+  const filterClasses = i => Boolean(i) && typeof i === "string" && !i.startsWith('svelte-');
 
   const configMerged = mergeUserConfig(getConfig(), userConfig);
   const parse = getParser(configMerged);
 
   const process = c => {
     const css = parse(c);
-    if (css) classes.add(c);
+    if (css) {
+      classes.add(c);
+    }
     return css;
   }
 
-  const classObserver = new MutationObserver(onObserve(process, filterClasses, appendStyle, appendStyleMedia));
-
-  observeClasses(classObserver, container);
   const initialStyles = getInitialClasses(process, filterClasses);
-
   appendStyle(preflight + variables(configMerged) + initialStyles.filter(s => !s.includes('@media')).join('\n') + keyframes);
   appendStyleMedia(initialStyles.filter(s => s.includes('@media')).join('\n'));
+
+  const classObserver = new MutationObserver(onObserve(process, filterClasses, appendStyle, appendStyleMedia));
+  observeClasses(classObserver, container);
 
   return {
     unsubscribe: () => classObserver.disconnect(),
